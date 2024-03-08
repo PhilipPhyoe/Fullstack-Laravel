d7eb4aa087e56f2404c3a36cf5c73659229e58a4
Philip Phyoe <pyaiephyoe@efectivee.com>
2024-03-08 20:42:42 +0630

    change product theme

--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -1,35 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
-<title>{{ $product->product_title }}</title>
+<title>{{ $product->name }}</title>
 
 <x-layout>
-    <section class="ml-4 mt-8 flex items-start flex-col">
-        <h1 class="font-semibold text-2xl mb-4">
-            <a href="/products/{{ $product->slug }}">
-                {{ $product->name }}
-            </a>
-        </h1>
-        <div class="flex gap-8 text-lg">
-            <a class=" hover:text-blue-700" href="/brands/{{ $product->brand->name }}">
-                {{ $product->brand->name }}
-            </a>
-
-            <a class=" hover:text-blue-700" href="/categories/{{ $product->category->slug }}">
-                {{ $product->category->name }}
-            </a>
+    <section class="m-16 flex justify-center gap-16">
+        <div class="w-[500px] h-[500px] flex justify-center items-center rounded-md shadow-md hover:scale-110">
+            <img class="w-full object-cover" src="/{{ $product->image_url }}" />
         </div>
-        <div class="flex flex-col gap-4 items-start text-lg mt-4">
-            <p>Product Description:
-                {{ $product->description }}
-            </p>
-            <p>Price:
-                {{ $product->unit_price }}
-            </p>
+        <div class="flex flex-col items-start gap-8">
+            <h1 class="font-semibold text-2xl mb-4">
+                <a href="/products/{{ $product->slug }}">
+                    {{ $product->name }}
+                </a>
+            </h1>
+            <div class="flex gap-8 text-lg">
+                <a class=" hover:text-blue-700" href="/brands/{{ $product->brand->name }}">
+                    {{ $product->brand->name }}
+                </a>
 
-            <a class=" hover:text-blue-700" href="/vehicles/{{ $product->vehicle->slug }}">
-                {{ $product->Vehicle->name }}
-            </a>
+                <a class=" hover:text-blue-700" href="/categories/{{ $product->category->slug }}">
+                    {{ $product->category->name }}
+                </a>
+            </div>
+            <div class="flex flex-col gap-4 items-start text-lg mt-4">
+                <p>Product Description: {{ $product->description }}
+                </p>
+                <p>Price: {{ $product->unit_price }} MMK
+                </p>
+                <p class="font-semibold text-lg">Suitable Vehicle</p>
+                <a class=" hover:text-blue-700" href="/vehicles/{{ $product->vehicle->slug }}">
+                    {{ $product->Vehicle->name }}
+                </a>
+            </div>
         </div>
+
     </section>
 </x-layout>
 
